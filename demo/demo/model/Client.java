40a25177d0fa5d6e657126e80b83edc42156e707
MaaTh4 <127776316+MaaTh4@users.noreply.github.com>
2023-12-06 11:42:29 -0300

    Add files via upload

---
+++ demo/demo/model/Client.java
@@ -0,0 +1,16 @@
+package AT_Java.demo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@AllArgsConstructor@NoArgsConstructor@Builder
+public class Client {
+    private Long Id;
+    private String name;
+    private String CPF;
+
+
+}
